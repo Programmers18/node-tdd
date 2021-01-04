f18c19cd431e71be54ff205252384cff7a2aca6d
Jonny D. Lezcano <jonnyda1811@gmail.com>
2021-01-04 16:41:57 -0300

    refactor: add functions for each property VehicleModel

---
+++ src/_tests_/register-vehicle.spec.ts
@@ -0,0 +1,30 @@
+import { RegisterVehicle } from '../controllers/register-vehicle'
+describe('RegisterVehicle', () => {
+  test('If the name does not exist return 400', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        // name: 'Nissan',
+        model: 'DXT',
+        year: 2020
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Error in the: name'))
+  })
+
+  test('If the model does not exist return 400', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        name: 'Nissan',
+        // model: 'DXT',
+        year: 2020
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Error in the: model'))
+  })
+})
