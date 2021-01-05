74070f390ae2f9abf6905ff11c40750d2c7b6a44
Jonny D. Lezcano <jonnyda1811@gmail.com>
2021-01-05 14:46:45 -0300

    test: add FoodModelHelper inject constructor FoodModel

--- src/_tests_/food-model.spec.ts
+++ src/_tests_/food-model.spec.ts
@@ -1,11 +1,17 @@
-import { FoodModel } from '../models/food-model'
+import { Food } from '../models/food-model'
 
 describe('FoodModel', () => {
   it('Class Food model', () => {
-    const foodModel = new FoodModel('Guiso', 'Un rico guiso con arroz y pollo', 12)
+    const food = new Food({
+      name: 'Guiso',
+      description: 'Un rico guiso con arroz y pollo',
+      price: 12,
+      inventory: 240
+    })
 
-    expect(foodModel.getName()).toEqual('Guiso')
-    expect(foodModel.getDescription()).toEqual('Un rico guiso con arroz y pollo')
-    expect(foodModel.getPrice()).toBe(12)
+    expect(food.getName()).toEqual('Guiso')
+    expect(food.getDescription()).toEqual('Un rico guiso con arroz y pollo')
+    expect(food.getPrice()).toBe(12)
+    expect(food.getInventory()).toBe(240)
   })
 })
